9a856fbab5d11c2a59eeb40bccd72b73881b0927
Toxicfy <15927630261@163.com>
2019-08-30 17:47:59 +0800

    :art: change: 修改登录页页面样式

--- back-server/util/auth.js
+++ back-server/util/auth.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken')
 const { privateKey } = require('./common')
 const AdminUser = require('../models/AdminUser')
+const ERROR_MESSAGE = {
+  401: '身份验证已过期，请重新登录'
+}
 
 module.exports = async (ctx, next) => {
   //  登录或者资源  -- 不校验
@@ -8,21 +11,23 @@ module.exports = async (ctx, next) => {
     String(ctx.originalUrl).indexOf('/api/') === -1 ||
     ctx.originalUrl === '/api/login' ||
     ctx.request.method === 'OPTIONS'
-
+  const authErrorStatus = 401
   if (notValid) await next()
   else {
     const token = String(ctx.request.headers.authorization || '')
       .split(' ')
       .pop()
-    ctx.assert(token, 401, '身份验证已过期，请重新登录')
-
-    const { id, time } = await jwt.verify(token, privateKey)
-    ctx.assert(
-      Date.now() - time > 1000 * 60 * 60,
-      401,
-      '身份验证已过期，请重新登录'
+      ctx.assert(token, authErrorStatus, ERROR_MESSAGE[authErrorStatus])
+      
+      const { id, time } = await jwt.verify(token, privateKey);
+      console.log(time)
+      ctx.assert(
+      Date.now() - time < 1000 * 60 * 60,
+      authErrorStatus,
+      ERROR_MESSAGE[authErrorStatus]
     )
-
+    const userModel = await AdminUser.findById(id)
+    ctx.assert(userModel, authErrorStatus, ERROR_MESSAGE[authErrorStatus])
     await next()
   }
 }
--- front-managent/src/views/Login.vue
+++ front-managent/src/views/Login.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="container">
-      <el-form :model="loginData" @submit.native.prevent="login('loginForm')" ref="loginForm" label-width="100px" :rules="rules">
-        <el-form-item prop="username" label="用户名">
-          <el-input v-model="loginData.username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password" label="密码">
-          <el-input type="password" v-model="loginData.password"></el-input>
-        </el-form-item>
-        <el-form-item>
+    <div class="center">
+      <!--img-->
+      <div class="img-container"></div>
+      <div class="form-container">
+        <!--title-->
+        <h2>登录</h2>
+        <!--form-->
+        <el-form :model="loginData" @submit.native.prevent="login('loginForm')" ref="loginForm" label-width="60px"
+                 :rules="rules">
+          <el-form-item prop="username" label="用户名">
+            <el-input v-model="loginData.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password" label="密码">
+            <el-input type="password" v-model="loginData.password"></el-input>
+          </el-form-item>
           <el-button type="info" native-type="submit">登录</el-button>
-        </el-form-item>
-      </el-form>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -60,17 +68,45 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .container {
+    background: #d4dadc;
     height: 100%;
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 
-    .el-form{
-      transform: translateY(-40%);
-      min-width: 375px;
+    .center {
+      display: flex;
+      height: 400px;
+      transform: translateY(-20%);
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+
+      .img-container{
+        z-index: 2;
+        width: 300px;
+        height: 400px;
+        background: url("../assets/cover.jpg") no-repeat center;
+        background-size: cover;
+        box-shadow: 3px 0 8px rgba(0, 0, 0, 0.2);
+      }
+      .form-container{
+        background: #ffffff;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        h2{
+          transform: translateY(-20px);
+        }
+        .el-form {
+          min-width: 375px;
+          padding: 20px;
+          box-sizing: border-box;
+          transform: translateY(-5%);
+        }
+      }
     }
   }
 </style>
